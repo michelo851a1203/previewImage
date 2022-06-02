a462a53d928ca8466ed48544b9b84917c2bb98a7
MichaelHo <michael@paradromix.com>
2022-06-02 15:28:21 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+  const currentImage = ref('')
+</script>
+
+<template>
+  <input type="file" name="" id="">
+
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
